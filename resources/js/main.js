0f698a9100fd9cb76728e3ee2236e2232fb227d5
Eduardo Rosillo <edrosillo@MacBook-Pro.local>
2018-05-28 11:57:21 -0400

    Added own files to Carousel in Design page.

--- resources/js/main.js
+++ resources/js/main.js
@@ -9,3 +9,12 @@ $(document).ready(function(){
     pauseOnHover: false
   });
   });
+
+(function ($) {
+  "use strict";
+
+  // manual carousel controls
+  $('.next').click(function () { $('.carousel').carousel('next'); return false; });
+  $('.prev').click(function () { $('.carousel').carousel('prev'); return false; });
+
+})(jQuery);
